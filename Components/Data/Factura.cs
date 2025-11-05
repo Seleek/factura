35bceab76d77c2217d1ec4839ebeb6c263c29574
Yatziri Cervantes <yicb_5@hotmail.com>
2025-11-05 13:40:54 -0700

    Se colocan los atributos de las facturas

---
+++ Components/Data/Factura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Factura
+
+{
+	public int Identificador { get; set; }
+
+	public DateTime Fecha { get; set; }
+
+	public string Nombre { get; set; }
+
+	public string Articulo { get; set; }
+
+	public double Precio { get; set; }
+
+
+
+}
---
+++ Components/Data/llenarFactura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class LlenarFactura
+{
+	private List<Factura> facturas = new List<Factura>
+	{
+		new Factura{Identificador = 1, Fecha = (2015,12,31), Nombre = "Maria", "Cepillo", Precio = 4},
+		new Factura{Identificador = 2, Fecha = (2016,11,31), Nombre = "Juan", "Peine", Precio = 6},
+	};
+
+	public Task<List<Factura>> ObtenerFacturas() => Task.FromResult(facturas);
+
+	public Task AgregarFactura(Factura factura)
+	{
+		facturas.add(factura);
+		return Task.CompletedTask;
+	}
+}
