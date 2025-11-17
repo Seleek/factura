3551ddcaa5661c11bad3a7edf354469901e6f8dd
Yatziri Cervantes <yicb_5@hotmail.com>
2025-11-17 11:28:42 -0700

    Se hizo el query para agregar y obtener elementos de la base de datos de facturas

--- Components/Data/llenarFactura.cs
+++ Components/Data/llenarFactura.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,17 +7,53 @@ namespace factura.Components.Data;
 
 public class LlenarFactura
 {
-    private List<Factura> facturas = new List<Factura>
-    {
-        new Factura { Identificador = 1, Fecha = new DateOnly(2015, 12, 31), Nombre = "Maria", Articulo = "Cepillo", Precio = 4 },
-        new Factura { Identificador = 2, Fecha = new DateOnly(2016, 11, 30), Nombre = "Juan", Articulo = "Peine", Precio = 6 },
-    };
+    private List<Factura> facturas = new List<Factura>();
 
-    public Task<List<Factura>> ObtenerFacturas() => Task.FromResult(facturas);
 
-    public Task AgregarFactura(Factura factura)
+
+    public async Task<List<Factura>> ObtenerFacturas()
     {
+        facturas.Clear();
+        String ruta = "facturas.db";
+        using var conexion = new SqliteConnection($"DataSource={ruta}");
+        await conexion.OpenAsync();
+
+        var comando = conexion.CreateCommand();
+        comando.CommandText = "SELECT ID,FECHA,NOMBRE,ARTICULO,PRECIO FROM FACTURAS";
+        using var lector = await comando.ExecuteReaderAsync();
+
+        while (await lector.ReadAsync())
+        {
+            facturas.Add(new Factura
+            {
+                Identificador = lector.GetInt32(0),
+                Fecha = DateOnly.FromDateTime(lector.GetDateTime(1)),
+                Nombre = lector.GetString(2),
+                Articulo = lector.GetString(3),
+                Precio = lector.GetInt32(4)
+            });
+        }
+        return facturas;
+
+    }
+    public async Task AgregarFactura(Factura factura)
+    {
+        String ruta = "facturas.db";
+        using var conexion = new SqliteConnection($"DataSource={ruta}");
+        await conexion.OpenAsync();
+
+        var comando = conexion.CreateCommand();
+
+        comando.CommandText = "insert into facturas (id,fecha,nombre,articulo,precio) values ($ID,$FECHA,$NOMBRE,$ARTICULO,$PRECIO)";
+        comando.Parameters.AddWithValue("$ID", factura.Identificador);
+        comando.Parameters.AddWithValue("$FECHA", factura.Fecha);
+        comando.Parameters.AddWithValue("$NOMBRE", factura.Nombre);
+        comando.Parameters.AddWithValue("$ARTICULO", factura.Articulo);
+        comando.Parameters.AddWithValue("$PRECIO", factura.Precio);
+        
+        comando.ExecuteNonQueryAsync();
+
         facturas.Add(factura);
-        return Task.CompletedTask;
+      
     }
 }
